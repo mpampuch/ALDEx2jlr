73c7fc982138a30dce6682fd0e59e464e8433540
mpampuch <markpampuch@gmail.com>
2025-12-25 09:30:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ALDEx2jlr
+
+This package serves as the R interface for the ALDEx2.jl and ALDEx2GPU.jl packages.
---
+++ tests/testthat/test-ALDEx2jlr.R
@@ -0,0 +1,15 @@
+test_that("Setup works", {
+  # Pass
+})
+
+test_that("Function wrapping works", {
+  # Pass
+})
+
+test_that("Type conversion works", {
+  # Pass
+})
+
+test_that("JIT compilation works", {
+  # Pass
+})
